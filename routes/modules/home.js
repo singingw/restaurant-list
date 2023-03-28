36bbc8ca4f0f4c7e0721d4a81bc1486bb6136fe4
singingw <op0408io1998@gmail.com>
2023-03-28 22:46:01 +0800

    增加 get('/')

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  const userId = req.user._id
+  Restaurant.find({ userId })
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
 module.exports = router
