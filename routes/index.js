06624c57b94ae989a84a704abc7829002ab03497
singingw <op0408io1998@gmail.com>
2023-03-28 21:16:59 +0800

    加 驗證登入狀態

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
