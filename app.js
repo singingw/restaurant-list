5b4eacef6214cc2922b1f60ad8c2d47700689a4f
singingw <op0408io1998@gmail.com>
2023-03-28 20:24:58 +0800

    部屬至 Heroku

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}//判別開發環境
+
 const app = express()
+const port = process.env.PORT
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -9,6 +15,6 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
 })
