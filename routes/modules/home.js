3f488aee105169423f20db40de0be6ee08774903
singingw <op0408io1998@gmail.com>
2023-03-28 23:26:41 +0800

    增加 篩選 與 排序 功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,5 +9,25 @@ router.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
+router.post('/filter', (req, res) => {
+  const { sort, category, rating } = req.body
+  const userId = req.user._id
+  const sortRule = {
+    default: { _id: 'asc' },
+    name_asc: { name: 'asc' },
+    name_desc: { name: 'desc' },
+    rating_asc: { rating: 'asc' },
+    rating_desc: { rating: 'desc' }
+  }
 
+  Restaurant.find({
+    $and: [{ userId },
+    { category: { $regex: category, $options: 'i' } },
+    { rating: { $gte: rating } }]
+  })
+    .sort(sortRule[sort])
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, sort, category, rating }))
+    .catch(error => console.log(error))
+})
 module.exports = router
